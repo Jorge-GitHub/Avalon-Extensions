79377185662e0a17531119407d22f2dfea47dfaa
George Gonzalez <84981824+GeorgeGonzalez@users.noreply.github.com>
2023-07-10 04:24:21 -0500

    Add ToListString to enums extensions.

---
+++ src/Avalon.Base.Extension/Avalon.Base.Extension/Types/EnumExtensions/EnumsListExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Avalon.Base.Extension.Collections;
+
+namespace Avalon.Base.Extension.Types.EnumExtensions;
+
+public static class EnumsListExtensions
+{
+    /// <summary>
+    /// Convert a list of enums into a list of strings.
+    /// </summary>
+    /// <param name="enums">
+    /// Enums to convert.
+    /// </param>
+    /// <returns>
+    /// list of strings.
+    /// </returns>
+    public static List<string> ToListString(this List<Enum> enums)
+    {
+        if (enums.HasElements())
+        {
+            return enums.Select(
+                value => value.ToString()).ToList();
+        }
+
+        return null;
+    }
+}
