7d2a1d057dabfdad707accc3ce5a3ecf25404db5
George Gonzalez <84981824+GeorgeGonzalez@users.noreply.github.com>
2023-11-23 17:31:59 -0600

    Add ToStringStringBuilder and UT.

--- src/Avalon.Base.Extension/Avalon.Base.Extension/Collections/IEnumerableCollectionBasicExtensions.cs
+++ src/Avalon.Base.Extension/Avalon.Base.Extension/Collections/IEnumerableCollectionBasicExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using Avalon.Base.Extension.Types;
+using System.Text;
 
 namespace Avalon.Base.Extension.Collections;
 
@@ -96,5 +97,36 @@ public static class IEnumerableCollectionBasicExtensions
     public static void Span<T>(this IEnumerable<T> list, Action<T> action)
     {
         list.ToList().Span(action);
+    }
+
+    /// <summary>
+    /// Convert a list into a string builder. Each element will be append as a new line.
+    /// </summary>
+    /// <typeparam name="T">
+    /// List type.
+    /// </typeparam>
+    /// <param name="list">
+    /// List with the items to perform the action.
+    /// </param>
+    /// <returns>
+    /// String builder. Each element will be append as a new line.
+    /// </returns>
+    public static StringBuilder ToStringStringBuilder<T>(this IEnumerable<T> list)
+    {
+        if (list.HasElements())
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (T item in list)
+            {
+                if (item.IsNotNull())
+                {
+                    result.AppendLine(item?.ToString());
+                }
+            }
+
+            return result;
+        }
+
+        return new StringBuilder();
     }
 }
