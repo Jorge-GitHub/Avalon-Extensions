a90ff6a5a6e6bc2c7b73a4d5390d26d376472a22
George Gonzalez <84981824+GeorgeGonzalez@users.noreply.github.com>
2023-12-04 20:39:58 -0600

    Add ToSafeSQL to ParameterExtensions plus UT.

---
+++ src/Avalon.Base.Extension/Avalon.Base.Extension.UT/Data/ParameterExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using Avalon.Base.Extension.Data;
+using Microsoft.Data.SqlClient;
+
+namespace Avalon.Base.Extension.UT.Data;
+
+/// <summary>
+/// String tests.
+/// </summary>
+[TestClass]
+public class ParameterExtensionsTest
+{
+    /// <summary>
+    /// Test the ToSafeSQL extension.
+    /// </summary>
+    [TestMethod]
+    public void ToSafeSQLTest()
+    {
+        List<SqlParameter> parameters = new List<SqlParameter>();
+        parameters.Add(new SqlParameter("@Paramater1", -1));
+        parameters.Add(new SqlParameter("@Paramater2", "Car"));
+        parameters.ToSafeSQL();
+        Assert.IsTrue(parameters.Count == 2);
+    }
+}
---
+++ src/Avalon.Base.Extension/Avalon.Base.Extension/Data/ParameterExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Avalon.Base.Extension.Collections;
+using Avalon.Base.Extension.Types;
+using Microsoft.Data.SqlClient;
+
+namespace Avalon.Base.Extension.Data;
+
+/// <summary>
+/// Parameters extensions.
+/// </summary>
+public static class ParameterExtensions
+{
+    /// <summary>
+    /// Cleans the  parameters for especial 
+    /// characters, to avoid SQL injection.
+    /// </summary>
+    /// <param name="parameters">
+    /// Store procedure parameters.
+    /// </param>
+    public static void ToSafeSQL(this List<SqlParameter> parameters)
+    {
+        if (parameters.HasElements())
+        {
+            foreach (SqlParameter parameter in parameters)
+            {
+                parameter.Value = @parameter.Value;
+            }
+        }
+    }
+}
