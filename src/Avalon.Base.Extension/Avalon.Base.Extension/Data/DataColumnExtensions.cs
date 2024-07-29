1cb438b44f13a43505d26f791dc4b67f6963539b
George Gonzalez <84981824+GeorgeGonzalez@users.noreply.github.com>
2024-07-29 01:04:14 -0500

    Add ToSafeString and ToSafeInteger extensions to DataColumns.

---
+++ src/Avalon.Base.Extension/Avalon.Base.Extension/Data/DataColumnExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Avalon.Base.Extension.Types;
+using System.Data;
+
+namespace Avalon.Base.Extension.Data;
+
+public static class DataColumnExtensions
+{
+    /// <summary>
+    /// Get the column's value and if the value is null it will returns an empty string.
+    /// </summary>
+    /// <param name="column">
+    /// Data column to get the value from.
+    /// </param>
+    /// <param name="row">
+    /// Data columns' row.
+    /// </param>
+    /// <returns>
+    /// Data column value as a string.
+    /// </returns>
+    public static string ToSafeString(this DataColumn column, DataRow row)
+    {
+        if (row != null && row[column] != null && row[column] != DBNull.Value) {
+            return row[column].ToSafeString();
+        }
+
+        return string.Empty;
+    }
+
+    /// <summary>
+    /// Get the column's value and if is empty returns the default value.
+    /// </summary>
+    /// <param name="column">
+    /// Data column to get the value from.
+    /// </param>
+    /// <param name="row">
+    /// Data columns' row.
+    /// </param>
+    /// <returns>
+    /// Data column value as an integer.
+    /// </returns>
+    public static int ToSafeInteger(this DataColumn column, 
+        DataRow row, int defaultValue = 0)
+    {
+        return column.ToSafeString(row).ToInteger(defaultValue);
+    }
+}
