238d69d5e1e5c40ae8d4ea0e5761ca010680a45a
George Gonzalez <84981824+GeorgeGonzalez@users.noreply.github.com>
2025-12-30 12:42:35 -0600

    Remove unused namespaces.

--- src/Avalon.Base.Extension/Avalon.Base.Extension/Types/StringExtensions/StringExtensions.cs
+++ src/Avalon.Base.Extension/Avalon.Base.Extension/Types/StringExtensions/StringExtensions.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Avalon.Base.Extension.Types.StringExtensions;
 
