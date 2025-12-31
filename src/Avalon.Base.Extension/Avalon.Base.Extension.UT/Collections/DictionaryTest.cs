924fc49d3f42fb2d83bcf66389b758bb5cb0cfb5
George Gonzalez <84981824+GeorgeGonzalez@users.noreply.github.com>
2025-12-31 12:29:55 -0600

    Add AddRange extensions to Dictionary and UTs.

---
+++ src/Avalon.Base.Extension/Avalon.Base.Extension.UT/Collections/DictionaryTest.cs
@@ -0,0 +1,60 @@
+﻿using Avalon.Base.Extension.Collections;
+
+namespace Avalon.Base.Extension.UT.Collections;
+
+/// <summary>
+/// Dictionary tests.
+/// </summary>
+[TestClass]
+public class DictionaryTest
+{
+    [TestMethod]
+    public void TestAddRange()
+    {
+        Dictionary<string, string[]> dictionaryTarget = new();
+        dictionaryTarget["UniqueElement"] = new[] { "Unique element." };
+        dictionaryTarget["DuplicateElement"] = new[] { "Duplicate element." };
+
+        Dictionary<string, string[]> dictionarySource = new();
+        dictionarySource["UniqueSourceElement"] = new[] { "Unique source element." };
+        dictionarySource["DuplicateElement"] = new[] { "Duplicate source element." };
+
+        dictionaryTarget.AddRange(dictionarySource);
+
+        Assert.IsTrue(dictionaryTarget.Count == 3);
+    }
+
+    [TestMethod]
+    public void TestAddRangeNotUpdateDuplicate()
+    {
+        Dictionary<string, string[]> dictionaryTarget = new();
+        dictionaryTarget["UniqueElement"] = new[] { "Unique element." };
+        dictionaryTarget["DuplicateElement"] = new[] { "Duplicate element." };
+
+        Dictionary<string, string[]> dictionarySource = new();
+        dictionarySource["UniqueSourceElement"] = new[] { "Unique source element." };
+        dictionarySource["DuplicateElement"] = new[] { "Duplicate source element." };
+
+        dictionaryTarget.AddRange(dictionarySource, updateValue: false);
+
+        Assert.AreEqual(
+            "Duplicate element.",
+            dictionaryTarget["DuplicateElement"][0]);
+    }
+
+    [TestMethod]
+    public void TestAddRangeThrowExceptionOnDuplicate()
+    {
+        Dictionary<string, string[]> dictionaryTarget = new();
+        dictionaryTarget["UniqueElement"] = new[] { "Unique element." };
+        dictionaryTarget["DuplicateElement"] = new[] { "Duplicate element." };
+
+        Dictionary<string, string[]> dictionarySource = new();
+        dictionarySource["UniqueSourceElement"] = new[] { "Unique source element." };
+        dictionarySource["DuplicateElement"] = new[] { "Duplicate element." };
+
+        Assert.ThrowsException<InvalidOperationException>(() =>
+           dictionaryTarget.AddRange(
+            dictionarySource, updateValue: false, throwExceptionOnDuplicate: true));
+    }
+}
---
+++ src/Avalon.Base.Extension/Avalon.Base.Extension/Collections/DictionaryExtensions.cs
@@ -0,0 +1,40 @@
+﻿namespace Avalon.Base.Extension.Collections;
+
+public static class DictionaryExtensions
+{
+    public static void AddRange<TKey, TValue>(
+    this Dictionary<TKey, TValue[]> target,
+    Dictionary<TKey, TValue[]> source,
+    bool updateValue = true,
+    bool throwExceptionOnDuplicate = false)
+    {
+        if (target == null)
+        {
+            throw new ArgumentNullException(nameof(target));
+        }
+        if (source?.Count > 0)
+        {
+            foreach (var element in source)
+            {
+                if (element.Key is not null)
+                {
+                    if (target.ContainsKey(element.Key))
+                    {
+                        if (throwExceptionOnDuplicate)
+                        {
+                            throw new InvalidOperationException($"Duplicate key found on target dictionary. Key: {element.Key}");
+                        }
+                        if (updateValue)
+                        {
+                            target[element.Key] = element.Value;
+                        }
+                    }
+                    else
+                    {
+                        target.Add(element.Key, element.Value);
+                    }
+                }
+            }
+        }
+    }
+}
