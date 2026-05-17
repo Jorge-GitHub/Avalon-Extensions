b176ae5c6e716015a35e380a1c89884133b1b120
George Gonzalez <84981824+GeorgeGonzalez@users.noreply.github.com>
2026-05-17 18:07:36 -0500

    Remove warnings

--- src/Avalon.Base.Extension/Avalon.Base.Extension.UT/Objects/User.cs
+++ src/Avalon.Base.Extension/Avalon.Base.Extension.UT/Objects/User.cs
@@ -5,9 +5,9 @@
 /// </summary>
 internal class User
 {
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public string LastName { get; set; }
-    public string Description { get; set; }
-    public string Password { get; set; }
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public string? LastName { get; set; }
+    public string? Description { get; set; }
+    public string? Password { get; set; }
 }
