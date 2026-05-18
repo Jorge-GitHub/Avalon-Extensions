eba580dbb41cf138626043d48844a8c348a48945
George Gonzalez <84981824+GeorgeGonzalez@users.noreply.github.com>
2026-05-17 19:22:12 -0500

    add compile mapping

--- src/Avalon.Base.Extension/Avalon.Base.Extension/Types/ObjectExtensions/ObjectMapExtensions.cs
+++ src/Avalon.Base.Extension/Avalon.Base.Extension/Types/ObjectExtensions/ObjectMapExtensions.cs
@@ -1,10 +1,15 @@
-﻿using System.Text.Json;
+using System.Collections.Concurrent;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Avalon.Base.Extension.Types.ObjectExtensions;
 
 public static class ObjectMapExtensions
 {
+    private static readonly ConcurrentDictionary<(Type SourceType, Type TargetType), Delegate> MapCache = new();
+
     private static readonly JsonSerializerOptions MapJsonOptions = new()
     {
         PropertyNameCaseInsensitive = true,
@@ -57,6 +62,85 @@ public static class ObjectMapExtensions
     /// </returns>
     public static T? Map<T>(this object? objectToMap)
     {
-        return objectToMap.Map<T>(MapJsonOptions);
+        if (objectToMap is not null)
+        {
+            var mapper = (Func<object, T>)MapCache.GetOrAdd(
+                (objectToMap.GetType(), typeof(T)),
+                static key => CreateMapper<T>(key.SourceType, key.TargetType));
+
+            return mapper(objectToMap);
+        }
+
+        return default;
+    }
+
+    private static Func<object, T> CreateMapper<T>(Type sourceType, Type targetType)
+    {
+        ParameterExpression sourceParameter = Expression.Parameter(typeof(object), "source");
+
+        if (targetType.IsAssignableFrom(sourceType))
+        {
+            UnaryExpression convertedSource = Expression.Convert(sourceParameter, targetType);
+
+            return Expression.Lambda<Func<object, T>>(
+                convertedSource,
+                sourceParameter).Compile();
+        }
+
+        ConstructorInfo? targetConstructor = targetType.GetConstructor(Type.EmptyTypes);
+        if (targetConstructor is null && !targetType.IsValueType)
+        {
+            throw new InvalidOperationException(
+                $"Type '{targetType.FullName}' must have a public parameterless constructor to be mapped.");
+        }
+
+        UnaryExpression typedSource = Expression.Convert(sourceParameter, sourceType);
+        NewExpression target = targetConstructor is null
+            ? Expression.New(targetType)
+            : Expression.New(targetConstructor);
+
+        MemberBinding[] bindings = CreatePropertyBindings(sourceType, targetType, typedSource);
+        MemberInitExpression body = Expression.MemberInit(target, bindings);
+
+        return Expression.Lambda<Func<object, T>>(
+            body,
+            sourceParameter).Compile();
+    }
+
+    private static MemberBinding[] CreatePropertyBindings(
+        Type sourceType,
+        Type targetType,
+        Expression typedSource)
+    {
+        Dictionary<string, PropertyInfo> sourceProperties = sourceType
+            .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+            .Where(property =>
+                property.CanRead &&
+                property.GetMethod?.IsPublic == true &&
+                property.GetIndexParameters().Length == 0)
+            .ToDictionary(
+                property => property.Name,
+                StringComparer.OrdinalIgnoreCase);
+
+        return targetType
+            .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+            .Where(property =>
+                property.CanWrite &&
+                property.SetMethod?.IsPublic == true &&
+                property.GetIndexParameters().Length == 0)
+            .Where(property =>
+                sourceProperties.TryGetValue(property.Name, out PropertyInfo? sourceProperty) &&
+                property.PropertyType.IsAssignableFrom(sourceProperty.PropertyType))
+            .Select(property =>
+            {
+                PropertyInfo sourceProperty = sourceProperties[property.Name];
+                MemberExpression sourceValue = Expression.Property(typedSource, sourceProperty);
+                Expression value = sourceValue.Type == property.PropertyType
+                    ? sourceValue
+                    : Expression.Convert(sourceValue, property.PropertyType);
+
+                return Expression.Bind(property, value);
+            })
+            .ToArray();
     }
 }
