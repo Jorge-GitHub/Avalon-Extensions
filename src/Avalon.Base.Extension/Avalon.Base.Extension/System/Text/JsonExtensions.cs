7d4e97949086d83d43e38b37e1baa242a4477d97
George Gonzalez <84981824+GeorgeGonzalez@users.noreply.github.com>
2026-05-25 01:26:59 -0500

    Improve ToParseJsonElementOrDefault

--- src/Avalon.Base.Extension/Avalon.Base.Extension/System/Text/JsonExtensions.cs
+++ src/Avalon.Base.Extension/Avalon.Base.Extension/System/Text/JsonExtensions.cs
@@ -7,16 +7,23 @@ public static class JsonExtensions
 {
     public static JsonElement ToParseJsonElementOrEmptyObject(this string? json)
     {
+        return json.ToParseJsonElementOrDefault(defaultJson: "{}");
+    }
+
+    public static JsonElement ToParseJsonElementOrDefault(
+        this string? json,
+        string defaultJson)
+    {
         try
         {
             using JsonDocument document = JsonDocument.Parse(
-                json.IsNullOrEmpty() ? "{}" : json);
+                json.IsNullOrEmpty() ? defaultJson : json);
 
             return document.RootElement.Clone();
         }
         catch (JsonException)
         {
-            using JsonDocument document = JsonDocument.Parse(json: "{}");
+            using JsonDocument document = JsonDocument.Parse(json: defaultJson);
 
             return document.RootElement.Clone();
         }
