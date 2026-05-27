bbb2c66c8d5c217b9bd2a3fccd9d72cabcdee291
George Gonzalez <84981824+GeorgeGonzalez@users.noreply.github.com>
2026-05-27 01:15:49 -0500

    Add TryParsePayload extension

--- src/Avalon.Base.Extension/Avalon.Base.Extension/System/Text/JsonExtensions.cs
+++ src/Avalon.Base.Extension/Avalon.Base.Extension/System/Text/JsonExtensions.cs
@@ -48,4 +48,23 @@ public static class JsonExtensions
 
         return value;
     }
+
+    public static JsonDocument? TryParsePayload(this string payloadJson)
+    {
+        JsonDocument? payload = null;
+
+        if (!string.IsNullOrWhiteSpace(payloadJson))
+        {
+            try
+            {
+                payload = JsonDocument.Parse(payloadJson);
+            }
+            catch (JsonException)
+            {
+                payload = null;
+            }
+        }
+
+        return payload;
+    }
 }
