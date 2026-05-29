cd657159683e6111f67caf5a97e304bb70bd44fe
George Gonzalez <84981824+GeorgeGonzalez@users.noreply.github.com>
2026-05-29 00:12:11 -0500

    Add NewId extensions.

---
+++ src/Avalon.Base.Extension/Avalon.Base.Extension/System/GuidExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Avalon.Base.Extension.System;
+
+public static class GuidExtensions
+{
+    public static string NewId(bool removeHyphens = true)
+    {
+        return Guid.NewGuid().ToString(removeHyphens ? "N" : "D");
+    }
+}
