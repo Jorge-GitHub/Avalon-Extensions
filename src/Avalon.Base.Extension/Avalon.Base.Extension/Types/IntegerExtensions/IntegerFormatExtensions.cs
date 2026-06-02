6c4077a85c367e09af1585505f9f69620f63dccf
George Gonzalez <84981824+GeorgeGonzalez@users.noreply.github.com>
2026-06-01 23:05:17 -0500

    fix extension method

--- src/Avalon.Base.Extension/Avalon.Base.Extension/Types/IntegerExtensions/IntegerFormatExtensions.cs
+++ src/Avalon.Base.Extension/Avalon.Base.Extension/Types/IntegerExtensions/IntegerFormatExtensions.cs
@@ -2,7 +2,7 @@
 
 public static class IntegerFormatExtensions
 {
-    public static int NormalizePositive(int value, int defaultValue = 0)
+    public static int NormalizePositive(this int value, int defaultValue = 0)
     {
         return value > 0 ? value : defaultValue;
     }
